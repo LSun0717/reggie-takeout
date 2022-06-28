5565a3fcf86e79b4a4ca02522b252a0e1cec50ea
LSun <LSun0717@163.com>
2022-06-28 18:10:24 +0800

    add MP pagination interceptor

---
+++ src/main/java/com/unclel/reggie/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.unclel.reggie.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName MybatisPlusConfig
+ * @Description MybatisPlus配置分页拦截器
+ * @Author uncle_longgggggg
+ * @Date 6/28/2022 5:39 PM
+ * @Version 1.0
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
