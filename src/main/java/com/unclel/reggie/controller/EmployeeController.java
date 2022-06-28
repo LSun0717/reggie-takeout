95ec6dbdf36092eef375c8cdb250fe8c0d7fd67f
LSun <LSun0717@163.com>
2022-06-28 18:12:47 +0800

    new feature:employee pagination queries

--- src/main/java/com/unclel/reggie/controller/EmployeeController.java
+++ src/main/java/com/unclel/reggie/controller/EmployeeController.java
@@ -1,16 +1,15 @@
 package com.unclel.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.unclel.reggie.common.R;
 import com.unclel.reggie.entity.Employee;
 import com.unclel.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -107,5 +106,32 @@ public class EmployeeController {
         log.info("新增员工， 员工信息：{}", employee.toString());
         employeeService.save(employee);
         return R.success("新增员工成功");
+    }
+
+    /*
+    * @description:分页查询
+    * @param page
+    * @param pageSize
+    * @param name
+    * @return: * @return R<Page>
+    * @author: uncle_longgggggg
+    * @time: 6/28/2022 5:44 PM
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+
+        log.info("page:{}, pageSzie, name:{}", page, pageSize, name);
+        // 构造分页构造器
+        Page pageInfo = new Page(page, pageSize);
+        // 构造条件构造器
+        LambdaQueryWrapper<Employee> employeeLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        // 添加过滤条件
+        employeeLambdaQueryWrapper.like(StringUtils.isNotEmpty(name), Employee::getName, name);
+        // 添加排序条件
+        employeeLambdaQueryWrapper.orderByDesc(Employee::getUpdateTime);
+        // 执行查询
+        employeeService.page(pageInfo, employeeLambdaQueryWrapper);
+
+        return R.success(pageInfo);
     }
 }
