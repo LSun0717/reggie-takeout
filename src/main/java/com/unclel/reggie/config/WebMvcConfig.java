23a9f0010fe6e816a44977b36b67c59a57d13d80
LSun <LSun0717@163.com>
2022-06-29 10:59:58 +0800

    extend MVC MessageConverter

--- src/main/java/com/unclel/reggie/config/WebMvcConfig.java
+++ src/main/java/com/unclel/reggie/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.unclel.reggie.config;
 
+import com.unclel.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 /**
  * @ClassName WebMvcConfig
@@ -23,5 +28,22 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("开始静态资源映射");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    /*
+    * @description:扩展MVC的消息转换器
+    * @param converters
+    * @return:
+    * @author: uncle_longgggggg
+    * @time: 6/29/2022 10:52 AM
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 创建消息转换器对象
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        // 设置对象转换器，底层使用Jackson将Java对象转为json
+        mappingJackson2HttpMessageConverter.setObjectMapper(new JacksonObjectMapper());
+        // 将上面的消息转换器对象追加到mvc框架的转换器集合中，其中idnex：0 表示将自定义转换器顺序设为第一
+        converters.add(0, mappingJackson2HttpMessageConverter);
     }
 }
